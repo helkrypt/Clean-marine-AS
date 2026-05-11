c9f32c83cf6c4c0257b2a1ebaf143fea10c7d335
helkrypt <marius@helkrypt.no>
2026-05-11 09:56:28 +0200

    Fjernet skip link

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,4 @@
-import { setRequestLocale, getTranslations } from "next-intl/server";
+import { setRequestLocale } from "next-intl/server";
 import { hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import Header from "@/components/layout/Header";
@@ -26,13 +26,8 @@ export default async function Home({
   }
   setRequestLocale(locale as Locale);
 
-  const t = await getTranslations("SkipLink");
-
   return (
     <>
-      <a href="#main" className="skip-link">
-        {t("text")}
-      </a>
       <Header />
       <main id="main">
         <Hero />
